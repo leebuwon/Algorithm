4697edb177a7e45242d20f91bf17b41a9ef594c7
leebuwon <97327583+leebuwon@users.noreply.github.com>
2026-03-03 10:25:46 +0900

    에너지 드링크

---
+++ baekjoon/silver/P20115.java
@@ -0,0 +1,35 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P20115 {
+    static int n;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        double sum = 0;
+        for(int i = 0; i < n - 1; i++) {
+            sum += (double) arr[i] / 2;
+        }
+
+        sum += arr[n - 1];
+
+
+        System.out.println(sum);
+    }
+}
